9202cd3c9cc5d38bc2265e4f098eabb3fd6d7c2c
Ivan Hroshev <grosheff.ivan@gmail.com>
2023-10-02 06:32:33 +0300

    commit-01

--- src/js/modules/timer.js
+++ src/js/modules/timer.js
@@ -1,3 +1,7 @@
+const blockHours = document.getElementById("hours");
+const blockMinutes = document.getElementById("minutes");
+const blockSecond = document.getElementById("seconds");
+
 function updateTimer() {
     const now = new Date();
     const endOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0, 0);
@@ -7,10 +11,12 @@ function updateTimer() {
     const minutes = Math.floor((timeRemaining / (1000 * 60)) % 60);
     const seconds = Math.floor((timeRemaining / 1000) % 60);
 
-    document.getElementById("hours").textContent = hours.toString().padStart(2, "0");
-    document.getElementById("minutes").textContent = minutes.toString().padStart(2, "0");
-    document.getElementById("seconds").textContent = seconds.toString().padStart(2, "0");
+    blockHours.textContent = hours.toString().padStart(2, "0");
+    blockMinutes.textContent = minutes.toString().padStart(2, "0");
+    blockSecond.textContent = seconds.toString().padStart(2, "0");
 }
 
-updateTimer(); // Обновляем таймер сразу
-setInterval(updateTimer, 1000); // Обновляем таймер каждую секунду
+if (blockHours && blockMinutes && blockSecond) {
+    updateTimer(); // Обновляем таймер сразу
+    setInterval(updateTimer, 1000); // Обновляем таймер каждую секунду
+}
